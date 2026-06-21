2e28a296481564ede49a98f350b05ffb6b856201
Mohd Ajmal <mohdajmalnayabazar@gmail.com>
2026-06-21 05:59:12 +0530

    Majority element

---
+++ Leetcode_solutions/majority_ele.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        for num in nums:
+            if count == 0:
+                current = num
+
+            if num == current:
+                count += 1
+
+            else:
+                count -= 1
+
+        return current
+
+
+
+
