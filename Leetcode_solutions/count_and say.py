83788df6243cd64adbd0520cd2fd3947b66f0e2b
Mohd Ajmal <mohdajmalnayabazar@gmail.com>
2026-06-21 11:00:04 +0530

    count and say

---
+++ Leetcode_solutions/count_and say.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        result = "1"
+        count = 0
+        for i in range(n - 1):
+            ans = ""
+            count = 0
+            current = result[0]
+            for ch in result:
+                if current != ch:
+                    ans = ans + str(count) + current
+                    count = 1
+                    current = ch
+                else:
+                    count += 1
+
+            ans = ans + str(count) + ch
+            result = ans
+        return result
