4932c29652b87e62ae3136feaa4a39df638d1dc3
Mohd Ajmal <mohdajmalnayabazar@gmail.com>
2026-06-25 22:33:00 +0530

    Best time to buy and sell stocks

---
+++ Leetcode_solutions/Best time to buy sell stocks.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        current_profit = 0
+        max_profit = 0
+        for p in prices:
+            if p < min_price:
+                min_price = p
+            current_profit = p - min_price
+            if max_profit < current_profit:
+                max_profit = current_profit
+
+        return max_profit
+
+
