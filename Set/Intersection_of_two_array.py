a1408b51f909e9ba11ea47c7c9cb0a4bf3ab6f9c
Mohd Ajmal <mohdajmalnayabazar@gmail.com>
2026-07-10 17:12:13 +0530

    Find distance value between two Array

---
+++ Set/Intersection_of_two_array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = set()
+        nums2 = set(nums2)
+
+        for i in nums1:
+            if i in nums2:
+                ans.add(i)
+
+        return list(ans)
+
+
+
+
+
+
+
+
+
+
