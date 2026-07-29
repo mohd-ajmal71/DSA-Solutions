daeac6a21b8952d23cd1ecfeb8010b503a0b1936
Mohd Ajmal <mohdajmalnayabazar@gmail.com>
2026-07-29 23:25:41 +0530

    Remove duplicate letter

--- Stack/Remove_duplicate_letter.py
+++ Stack/Remove_duplicate_letter.py
@@ -1,26 +1,30 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        fre_arr = [0] * 26
+
+        freq = [0] * 26
         seen = [False] * 26
         stack = []
+
         for ch in s:
             idx = ord(ch) - 97
-            fre_arr[idx] += 1
+            freq[idx] += 1
 
-        for i in range(len(s)):
-            idx = ord(s[i]) - 97
+        for ch in s:
+            idx = ord(ch) - 97
+            freq[idx] -= 1
 
-            if seen[idx] == True:
-                fre_arr[idx] -= 1
+            if seen[idx]:
                 continue
-            fre_arr[idx] -= 1
-            while stack and stack[-1] > s[i] and fre_arr[ord(stack[-1]) - 97] > 0:
-                idx1 = ord(stack[-1]) - 97
-                seen[idx1] = False
+
+            while stack and stack[-1] > ch and freq[ord(stack[-1]) - 97] > 0:
+                index = ord(stack[-1]) - 97
+                seen[index] = False
                 stack.pop()
 
-            stack.append(s[i])
-            seen[idx] = True
+            if seen[idx] == False:
+                stack.append(ch)
+                seen[idx] = True
 
         return "".join(stack)
+
 
